3ccd0577d25f4cc495c55be90fc9d3fcd80363e6
Nicole Chang <powernicole2@yahoo.com.tw>
2022-07-09 10:52:54 +0800

    feat: product components of Home (Lesson 5-7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import styles from "./App.module.scss";
-import { Header, Footer, Carousel, SideMenu } from "./components";
-import { Row, Col } from "antd";
+import theme from "./Theme.module.scss";
+import {
+  Header,
+  Footer,
+  Carousel,
+  SideMenu,
+  ProductCollection,
+} from "./components";
+import { Row, Col, Typography } from "antd";
+import { suggestProductList } from "./mockup";
+import { newProductList } from "./mockup";
+import { domesticProductList } from "./mockup";
+import suggestProductImg from "./assets/img/suggest_products.png";
+import newProductImg from "./assets/img/new_products.png";
+import domesticProductImg from "./assets/img/domestic_products.png";
 
 function App() {
   return (
@@ -16,6 +29,33 @@ function App() {
             <Carousel />
           </Col>
         </Row>
+        <ProductCollection
+          title={
+            <Typography.Title level={3} className={theme["text-theme"]}>
+              熱銷推薦
+            </Typography.Title>
+          }
+          sideImage={suggestProductImg}
+          products={suggestProductList}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} className={theme["text-theme"]}>
+              新品上市
+            </Typography.Title>
+          }
+          sideImage={newProductImg}
+          products={newProductList}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} className={theme["text-theme"]}>
+              國內旅遊
+            </Typography.Title>
+          }
+          sideImage={domesticProductImg}
+          products={domesticProductList}
+        />
       </div>
       <Footer />
     </div>
---
+++ src/components/productCollection/ProductImage.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Image, Typography } from "antd";
+import theme from "../../Theme.module.scss";
+
+interface PropsType {
+  id: string | number;
+  size: "large" | "small";
+  imageSrc: string;
+  price: string | number;
+  title: string;
+}
+
+export const ProductImage: React.FC<PropsType> = ({
+  id,
+  size,
+  imageSrc,
+  price,
+  title,
+}) => {
+  return (
+    <>
+      {size === "large" ? (
+        <Image src={imageSrc} height={285} width={490} />
+      ) : (
+        <Image src={imageSrc} height={120} width={240} />
+      )}
+      <div>
+        <Typography.Text className={theme["text-theme"]} strong>
+          {title.slice(0, 25)}
+        </Typography.Text>
+        <Typography.Text type="secondary">$ {price} 起</Typography.Text>
+      </div>
+    </>
+  );
+};
