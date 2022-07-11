51f10d2d062590df88a4c0392bd8ec6998a7c2e0
Nicole Chang <powernicole2@yahoo.com.tw>
2022-07-11 12:24:47 +0800

    refactor: extract code as HomePage and set it as a Route (Lesson 7-3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,16 @@
 import React from "react";
 import styles from "./App.module.scss";
-import theme from "./Theme.module.scss";
-import {
-  Header,
-  Footer,
-  Carousel,
-  SideMenu,
-  ProductCollection,
-} from "./components";
-import { Row, Col, Typography } from "antd";
-import { suggestProductList } from "./mockup";
-import { newProductList } from "./mockup";
-import { domesticProductList } from "./mockup";
-import suggestProductImg from "./assets/img/suggest_products.png";
-import newProductImg from "./assets/img/new_products.png";
-import domesticProductImg from "./assets/img/domestic_products.png";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div>
-      <Header />
-      <div className={styles["content-box"]}>
-        <Row className={styles["row"]}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={
-            <Typography.Title level={3} className={theme["text-theme"]}>
-              熱銷推薦
-            </Typography.Title>
-          }
-          sideImage={suggestProductImg}
-          products={suggestProductList}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} className={theme["text-theme"]}>
-              新品上市
-            </Typography.Title>
-          }
-          sideImage={newProductImg}
-          products={newProductList}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} className={theme["text-theme"]}>
-              國內旅遊
-            </Typography.Title>
-          }
-          sideImage={domesticProductImg}
-          products={domesticProductList}
-        />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import theme from "../../Theme.module.scss";
+import styles from "./HomePage.module.scss";
+import {
+  Header,
+  Footer,
+  Carousel,
+  SideMenu,
+  ProductCollection,
+} from "../../components";
+import { Row, Col, Typography } from "antd";
+import { suggestProductList } from "./mockup";
+import { newProductList } from "./mockup";
+import { domesticProductList } from "./mockup";
+import suggestProductImg from "../../assets/img/suggest_products.png";
+import newProductImg from "../../assets/img/new_products.png";
+import domesticProductImg from "../../assets/img/domestic_products.png";
+export class HomePage extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div className={styles["content-box"]}>
+          <Row className={styles["row"]}>
+            <Col span={6}>
+              <SideMenu />
+            </Col>
+            <Col span={18}>
+              <Carousel />
+            </Col>
+          </Row>
+          <ProductCollection
+            title={
+              <Typography.Title level={3} className={theme["text-theme"]}>
+                熱銷推薦
+              </Typography.Title>
+            }
+            sideImage={suggestProductImg}
+            products={suggestProductList}
+          />
+          <ProductCollection
+            title={
+              <Typography.Title level={3} className={theme["text-theme"]}>
+                新品上市
+              </Typography.Title>
+            }
+            sideImage={newProductImg}
+            products={newProductList}
+          />
+          <ProductCollection
+            title={
+              <Typography.Title level={3} className={theme["text-theme"]}>
+                國內旅遊
+              </Typography.Title>
+            }
+            sideImage={domesticProductImg}
+            products={domesticProductList}
+          />
+        </div>
+        <Footer />
+      </>
+    );
+  }
+}
