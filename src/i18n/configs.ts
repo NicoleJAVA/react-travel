1336111074d3f3ef882c0ab3e8788e0033e5cacf
Nicole Chang <powernicole2@yahoo.com.tw>
2022-07-15 22:02:44 +0800

    feat: i18n config settings (Lesson 8-7)

---
+++ src/i18n/configs.ts
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import translation_en from "./en.json";
+import translation_zh from "./zh.json";
+
+const resources = {
+  en: {
+    translation: translation_en,
+  },
+  zh: {
+    translation: translation_zh,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "zh",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+  },
+});
+
+export default i18n;
