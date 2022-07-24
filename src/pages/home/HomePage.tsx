648d0d87bfc10465ed9809521b8c04318ed87f5f
Nicole Chang <powernicole2@yahoo.com.tw>
2022-07-24 21:26:23 +0800

    feat(Home): fetch product list api (Lesson 9-4)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -8,17 +8,74 @@ import {
   SideMenu,
   ProductCollection,
 } from "../../components";
-import { Row, Col, Typography } from "antd";
-import { suggestProductList } from "./mockup";
-import { newProductList } from "./mockup";
-import { domesticProductList } from "./mockup";
+import { Row, Col, Typography, Spin } from "antd";
 import suggestProductImg from "../../assets/img/suggest_products.png";
 import newProductImg from "../../assets/img/new_products.png";
 import domesticProductImg from "../../assets/img/domestic_products.png";
 import { withTranslation, WithTranslation } from "react-i18next";
-class HomePageComponent extends React.Component<WithTranslation> {
+import axios from "axios";
+
+interface State {
+  loading: boolean;
+  error: string | null;
+  productList: any[];
+}
+class HomePageComponent extends React.Component<WithTranslation, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      productList: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get(
+        "http://123.56.149.216:8089/api/productCollections"
+      );
+      this.setState({
+        loading: false,
+        error: null,
+        productList: data,
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        this.setState({
+          error: err.message,
+          loading: false,
+        });
+      }
+    }
+  }
+
   render() {
     const { t } = this.props;
+    const { productList, loading, error } = this.state;
+
+    if (loading) {
+      return (
+        <Spin
+          size="large"
+          style={{
+            marginTop: 200,
+            marginBottom: 200,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "100%",
+          }}
+        />
+      );
+    }
+
+    if (error) {
+      return <div>網站發生錯誤： {error}</div>;
+    }
+
+    const suggestProductList = productList[0].touristRoutes;
+    const newProductList = productList[1].touristRoutes;
+    const domesticProductList = productList[2].touristRoutes;
 
     return (
       <>
