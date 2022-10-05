f272b9d10109260bf6fdef9d3715d9fc74d67891
Nicole Chang <powernicole2@yahoo.com.tw>
2022-10-05 16:37:02 +0800

    Revert "fix: remove the Github repository name from the homepage"
    This reverts commit ac78df37f236f6e8f709460bb56f88980e3bf15b.

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <div>
-      <HashRouter>
+      <HashRouter basename="/react-travel">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
