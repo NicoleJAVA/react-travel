103793aae60fb117aa209c4f6601e6338b080926
Nicole Chang <powernicole2@yahoo.com.tw>
2022-12-01 13:09:27 +0800

    feat: checkout button

--- src/pages/checkoutPage/CheckoutPage.tsx
+++ src/pages/checkoutPage/CheckoutPage.tsx
@@ -2,9 +2,9 @@ import styles from "./CheckoutPage.module.scss";
 import { MainLayout } from "../../layout/mainLayout";
 import { Spin } from "antd";
 import { useSelector, useAppDispatch } from "../../redux/hooks";
-import { API_SOURCE, UDEMY } from '../../helpers/constants';
-import { useEffect } from 'react';
-import { getOrder, payOrder } from '../../redux/order/slice';
+import { API_SOURCE, UDEMY } from "../../helpers/constants";
+import { useEffect } from "react";
+import { getOrder, payOrder } from "../../redux/order/slice";
 import { useParams } from "react-router-dom";
 
 type MatchParams = {
@@ -41,67 +41,91 @@ export const CheckoutPage: React.FC = () => {
               <th className={styles["text-end"]}>單價</th>
             </thead>
             <tbody>
-              {!isUdemy && order &&
-                Object.keys(order).map(o => order[o])
+              {!isUdemy &&
+                order &&
+                orderMeta &&
+                orderMeta.order &&
+                Object.keys(order)
+                  .map((o) => order[o])
                   .map((item, i) => (
-
-                    <tr key={`order-item-${i}`} className={styles["order-item"]}>
+                    <tr
+                      key={`order-item-${i}`}
+                      className={styles["order-item"]}
+                    >
                       <td>{item.product.title}</td>
                       <td className={styles["text-end"]}>{item.qty}</td>
-                      <td className={styles["text-end"]}>${item.final_total}</td>
+                      <td className={styles["text-end"]}>
+                        ${item.final_total}
+                      </td>
                     </tr>
                   ))}
-
-
             </tbody>
             <tfoot>
               <tr>
-                <td colSpan={2} className={styles["order-total-label"]}>總計</td>
-                {orderMeta && orderMeta.order && <td className={styles["order-total"]}>${orderMeta.order.total}</td>}
+                <td colSpan={2} className={styles["order-total-label"]}>
+                  總計
+                </td>
+                {orderMeta && orderMeta.order && (
+                  <td className={styles["order-total"]}>
+                    ${orderMeta.order.total}
+                  </td>
+                )}
               </tr>
             </tfoot>
           </table>
 
+          {orderUser && (
+            <table className={styles["data-table"]}>
+              <tbody>
+                <tr>
+                  <th className={styles["user-column-label"]}>Email</th>
+                  <td>{orderUser.email}</td>
+                </tr>
+                <tr>
+                  <th className={styles["user-column-label"]}>姓名</th>
+                  <td>{orderUser.name}</td>
+                </tr>
+                <tr>
+                  <th className={styles["user-column-label"]}>收件人電話</th>
+                  <td>{orderUser.tel}</td>
+                </tr>
+                <tr>
+                  <th className={styles["user-column-label"]}>收件人地址</th>
+                  <td>{orderUser.address}</td>
+                </tr>
+                <tr>
+                  <th className={styles["user-column-label"]}>付款狀態</th>
 
-          {orderUser && <table className={styles["data-table"]}>
-            <tbody>
-              <tr>
-                <th className={styles["user-column-label"]}>Email</th>
-                <td>{orderUser.email}</td>
-              </tr>
-              <tr>
-                <th className={styles["user-column-label"]}>姓名</th>
-                <td>{orderUser.name}</td>
-              </tr>
-              <tr>
-                <th className={styles["user-column-label"]}>收件人電話</th>
-                <td>{orderUser.tel}</td>
-              </tr>
-              <tr>
-                <th className={styles["user-column-label"]}>收件人地址</th>
-                <td>{orderUser.address}</td>
-              </tr>
-              <tr >
-                <th className={styles["user-column-label"]}>付款狀態</th>
-
-                <td>
-                  {orderMeta.order.is_paid && <span className={styles["paid"]} >付款完成</span>}
-                  {!orderMeta.order.is_paid && <span className={styles["not-paid"]} >尚未付款</span>}
-                </td>
-              </tr>
-            </tbody>
-            {/* <tfoot>
+                  <td>
+                    {orderMeta &&
+                      orderMeta.order &&
+                      orderMeta.order.is_paid && (
+                        <span className={styles["paid"]}>付款完成</span>
+                      )}
+                    {orderMeta &&
+                      orderMeta.order &&
+                      !orderMeta.order.is_paid && (
+                        <span className={styles["not-paid"]}>尚未付款</span>
+                      )}
+                  </td>
+                </tr>
+              </tbody>
+              {/* <tfoot>
             <tr>
 
             </tr>
           </tfoot> */}
-          </table>}
+            </table>
+          )}
           <div className={styles["pay-btn-container"]}>
-            {!orderMeta.order.is_paid && <div className={styles["pay-btn"]}
-              onClick={() => onPayOrder()}>確認付款去</div>}
+            {orderMeta && orderMeta.order && !orderMeta.order.is_paid && (
+              <div className={styles["pay-btn"]} onClick={() => onPayOrder()}>
+                確認付款去
+              </div>
+            )}
           </div>
         </div>
       </Spin>
-    </MainLayout >
+    </MainLayout>
   );
-}
+};
--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -8,16 +8,16 @@ import { useState } from "react";
 import {
   updateCart,
   deleteShoppingCartItems,
-  applyCoupon
+  applyCoupon,
 } from "../../redux/shoppingCart/slice";
 import { useNavigate } from "react-router-dom";
-import { API_SOURCE, UDEMY } from '../../helpers/constants';
+import { API_SOURCE, UDEMY } from "../../helpers/constants";
 
 const isUdemy = API_SOURCE === UDEMY;
 
 export const ShoppingCart: React.FC = () => {
   const loadingCart = useSelector((s) => s.shoppingCart.loading);
-  const shoppingCart = useSelector((s) => s.shoppingCart.items)
+  const shoppingCart = useSelector((s) => s.shoppingCart.items);
   const [couponCode, setCouponCode] = useState<string>("");
 
   let shoppingCartItems;
@@ -29,30 +29,30 @@ export const ShoppingCart: React.FC = () => {
 
   const onCartPlus = (cart) => {
     onUpdateCart(cart, cart.qty + 1);
-  }
+  };
 
   const onCartMinus = (cart) => {
     if (cart.qty <= 1) return;
 
     onUpdateCart(cart, cart.qty - 1);
-  }
+  };
 
   const onUpdateCart = (cart, quantity) => {
     const cartData = {
       cartId: cart.id,
       productId: cart.product.id,
       qty: quantity,
-    }
+    };
     dispatch(updateCart(cartData));
-  }
+  };
 
   const updateCouponCode = (e) => {
     setCouponCode(e.target.value);
-  }
+  };
 
   const onApplyCoupon = () => {
     dispatch(applyCoupon({ couponCode: couponCode }));
-  }
+  };
 
   const jwt = useSelector((s) => s.user.token);
   const dispatch = useAppDispatch();
@@ -66,7 +66,8 @@ export const ShoppingCart: React.FC = () => {
           <Col span={16}>
             <div className={styles["product-list-container"]}>
               {/* {isUdemy && <ProductList data={shoppingCartItems.map((s) => s.touristRoute)} />} */}
-              {!isUdemy && shoppingCartItems &&
+              {!isUdemy &&
+                shoppingCartItems &&
                 shoppingCartItems.map((item, i) => (
                   <div key={`cart-item-${i}`} className={styles["cart-item"]}>
                     <div className={styles["cart-item-text"]}>
@@ -78,14 +79,18 @@ export const ShoppingCart: React.FC = () => {
                       <div className={styles["cart-item-divider"]}></div>
                       <div className={styles["cart-item-second-row"]}>
                         <div className={styles["cart-item-qty-wrap"]}>
-                          <div className={theme["mb-1"]}>
-                            數量： {item.qty}
-                          </div>
+                          <div className={theme["mb-1"]}>數量： {item.qty}</div>
                           <div className={styles["cart-item-qty-buttons"]}>
-                            <div className={styles["left-rounded"]} onClick={() => onCartMinus(item)}>
+                            <div
+                              className={styles["left-rounded"]}
+                              onClick={() => onCartMinus(item)}
+                            >
                               -
                             </div>
-                            <div className={styles["right-rounded"]} onClick={() => onCartPlus(item)}>
+                            <div
+                              className={styles["right-rounded"]}
+                              onClick={() => onCartPlus(item)}
+                            >
                               +
                             </div>
                           </div>
@@ -95,31 +100,20 @@ export const ShoppingCart: React.FC = () => {
                         <div>折扣價 ${item.final_total}</div>
                       </div>
                     </div>
-                    <div className={styles["cart-item-img"]}>
-
-                    </div>
+                    <div className={styles["cart-item-img"]}></div>
                   </div>
-                ))
-              }
+                ))}
             </div>
           </Col>
           {/* payment */}
           <Col span={8}>
-
-
-
-
             <div className={styles["cart-checkout-section"]}>
-              <div className={styles["cart-checkout-title"]}>
-                CHECKOUT
-              </div>
+              <div className={styles["cart-checkout-title"]}>CHECKOUT</div>
 
               {/* row */}
               <div className={styles["checkout-row"]}>
                 <div className={styles["checkout-left-col"]}>
-                  <div className={styles["checkout-label"]}>
-                    總計
-                  </div>
+                  <div className={styles["checkout-label"]}>總計</div>
                 </div>
                 <div className={styles["checkout-right-col"]}>
                   $ {shoppingCart.total}
@@ -129,9 +123,7 @@ export const ShoppingCart: React.FC = () => {
               {/* row */}
               <div className={styles["checkout-row"]}>
                 <div className={styles["checkout-left-col"]}>
-                  <div className={styles["checkout-label"]}>
-                    折扣價
-                  </div>
+                  <div className={styles["checkout-label"]}>折扣價</div>
                 </div>
                 <div className={styles["checkout-right-col"]}>
                   $ {shoppingCart.final_total}
@@ -141,14 +133,43 @@ export const ShoppingCart: React.FC = () => {
               {/* row */}
               <div className={styles["checkout-row"]}>
                 <div className={styles["checkout-left-col"]}>
-                  <input type="text" className={styles["coupon-input"]} placeholder="請輸入優惠碼" onChange={updateCouponCode} />
+                  <input
+                    type="text"
+                    className={styles["coupon-input"]}
+                    placeholder="請輸入優惠碼"
+                    onChange={updateCouponCode}
+                  />
                 </div>
                 <div className={styles["checkout-right-col"]}>
-                  <div className={styles["coupon-btn"]} onClick={() => onApplyCoupon()}>套用優惠碼</div>
+                  <div
+                    className={styles["coupon-btn"]}
+                    onClick={() => onApplyCoupon()}
+                  >
+                    套用優惠碼
+                  </div>
+                </div>
+              </div>
+
+              {/* row */}
+              <div className={styles["checkout-row"]}>
+                {/* <div className={styles["checkout-left-col"]}>
+                  <input
+                    type="text"
+                    className={styles["coupon-input"]}
+                    placeholder="請輸入優惠碼"
+                    onChange={updateCouponCode}
+                  />
+                </div> */}
+                <div className={styles["checkout-btn"]}>
+                  <div
+                    className={styles["coupon-btn"]}
+                    onClick={() => navigate("/userForm")}
+                  >
+                    結帳
+                  </div>
                 </div>
               </div>
             </div>
-
           </Col>
         </Row>
       </Spin>
