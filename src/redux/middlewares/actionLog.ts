91d720e78ae893ee7ffb967115ae994e18ee339c
Nicole Chang <powernicole2@yahoo.com.tw>
2023-02-18 14:28:56 +0800

    refactor: remove redux logs

--- src/redux/middlewares/actionLog.ts
+++ src/redux/middlewares/actionLog.ts
@@ -3,7 +3,7 @@ import { json } from "../../helpers/logHelper";
 
 export const actionLog: Middleware = (store) => (next) => (action) => {
   // console.log("目前 store 狀態", json(store.getState()));
-  console.log("要派發的 action ", action);
+  // console.log("要派發的 action ", action);
   next(action);
   // console.log("更新後的 store 狀態", json(store.getState()));
 };
